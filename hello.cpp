c401c522609f2400f664f91ca4002b57722d2a19
hesam nemati <hesam.2905@gmail.com>
2022-11-25 12:33:10 +0330

    "add base file"

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // ask the user for his/her name
+
+    // get user's name and store it in a string variable
+
+    // greet user using the format "Hello, user!"
+
+    return 0;
+}
